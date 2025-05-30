6e034c9df0a57f0dd27dc7636b59416eece21379
mafutka <mafutka@gmail.com>
2025-05-30 21:00:41 +0200

    problems-resolved

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -6,7 +6,11 @@ const myToken = import.meta.env.VITE_TMDB_API_TOKEN;
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
   const response = await axios.get(`${BASE_URL}/search/movie`, {
-    params: { query },
+    params: { 
+      query,
+      language: 'en-US',
+      include_adult: false,
+},
     headers: {
       Authorization: `Bearer ${myToken}`,
     },
