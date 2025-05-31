1801fd419d0a4bfe50eb97193a8fbb13076ffba0
mafutka <mafutka@gmail.com>
2025-05-31 19:39:41 +0200

    movieService-edited

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,11 +1,18 @@
 import axios from 'axios';
 import type { Movie } from '../types/movie';
 
+interface ResponseMovie {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
 const BASE_URL = 'https://api.themoviedb.org/3';
 const myToken = import.meta.env.VITE_TMDB_API_TOKEN;
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
-  const response = await axios.get(`${BASE_URL}/search/movie`, {
+  const response = await axios.get<ResponseMovie>(`${BASE_URL}/search/movie`, {
     params: { 
       query,
       language: 'en-US',
